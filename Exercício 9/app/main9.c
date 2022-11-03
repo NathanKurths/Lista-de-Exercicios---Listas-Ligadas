d72cbd11f536127b1970a4711c98d0454808a732
NathanKurths <nathankurthsm@gmail.com>
2022-11-03 17:06:46 -0400

    corrigindo

--- Exercício 9/app/main9.c
+++ Exercício 9/app/main9.c
@@ -15,9 +15,9 @@ int main()
     LIsta *sta;
     criar_lista(li);
     cria_lista(sta);
-    insere_lista_ordenada(li, um);
-    insere_lista_ordenada(li, dois);
-    insere_lista_ordenada(sta, tres);
+    insere_lista_ordenada_aluno(li, um); //inserindo um carro de um aluno que respeita 90% da vaga de 200
+    insere_lista_ordenada_aluno(li, dois);
+    insere_lista_ordenada(sta, tres); // inserindo carro do funcionario que pelo menos terá 10% da vaga de 200
     insere_lista_ordenada(sta, quatro);
     print_lista(li);
     print_lista(sta);
